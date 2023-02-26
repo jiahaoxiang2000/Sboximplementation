bbee30afdf6c0855188bfbc62c1ae8ce860566a1
fjyxzm <119271060+fjyxzm@users.noreply.github.com>
2023-02-26 13:03:00 +0800

    Create newASCONsbox.v

---
+++ ASCON/Sofeware/sbox/newASCONsbox.v
@@ -0,0 +1,45 @@
+/**/
+#include <stdio.h>
+#include "time.h"
+#include <vcruntime_string.h>
+
+int main() {
+    
+    //int ll, i;
+    //for (ll = 0; ll <= 40; ll++) {
+        //clock_t start = clock();
+        //for (i = 0; i < 1000000; i++) {
+
+            int x0, x1, x2, x3, x4, y0, y1, y2, y3, y4;
+            x0 = 0x0000ffff;
+            x1 = 0x00ff00ff;
+            x2 = 0x0f0f0f0f;
+            x3 = 0x33333333;
+            x4 = 0x55555555;
+            y0 = x3 ^ x4;
+            y4 = x1 ^ x2;
+            y3 = y0 | x0;
+            y1 = y0 & x4;
+            y3 ^= y4;
+            y2 = (~y4) ^ y1;
+            y1 = y4 | x3;
+            y4 &= x2;
+            y4 ^= x0;
+            y1 ^= x2;
+            y4 ^= x4;
+            y1 ^= y4;
+            y0 ^= (y4 | x1);
+            y4 ^= y0;
+
+        //}
+
+
+        //sboxnew();
+
+        //clock_t  stop = clock();
+       // double duration = ((double)(stop - start)) / CLK_TCK;
+       // printf("%d   time:%f\n", ll, duration);
+    //}
+    return 0;
+
+}
